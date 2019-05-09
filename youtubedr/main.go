a8e96e1d3609ba4dd29e3266fde3410951e7b0e2
Andres Villagra <andres.villagra@alumnos.unc.edu.ar>
2019-05-09 08:54:07 -0300

    Added file location flags and a help string.
    Sometimes one wants to download to another directory other
    than the default.
    
    Also a better help string helps with using the program. :)

--- youtubedr/main.go
+++ youtubedr/main.go
@@ -10,19 +10,33 @@ import (
 	. "github.com/kkdai/youtube"
 )
 
+const usageString string = `Usage: youtubedr [OPTION] [URL]
+Download a video from youtube.
+Example: youtubedr -o "Campaign Diary".mp4 https://www.youtube.com/watch\?v\=XbNghLqsVwU
+`
+
 func main() {
+	flag.Usage = func() {
+		fmt.Println(usageString)
+		flag.PrintDefaults()
+	}
+	usr, _ := user.Current()
+	var outputFile string
+	flag.StringVar(&outputFile, "o", "dl.mp4", "The output file")
+	var outputDir string
+	flag.StringVar(&outputDir, "d",
+		filepath.Join(usr.HomeDir, "Movies", "yotubedr"),
+		"The output directory.")
 	flag.Parse()
 	log.Println(flag.Args())
-	usr, _ := user.Current()
-	currentDir := fmt.Sprintf("%v/Movies/youtubedr", usr.HomeDir)
-	log.Println("download to dir=", currentDir)
+	log.Println("download to dir=", outputDir)
 	y := NewYoutube(true)
 	arg := flag.Arg(0)
 	if err := y.DecodeURL(arg); err != nil {
 		fmt.Println("err:", err)
 		return
 	}
-	if err := y.StartDownload(filepath.Join(currentDir, "dl.mp4")); err != nil {
+	if err := y.StartDownload(filepath.Join(outputDir, outputFile)); err != nil {
 		fmt.Println("err:", err)
 	}
 }
