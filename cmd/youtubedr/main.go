9c0e4655c002740b0e1f9cce5a79129105dfb0ae
Julian Kornberger <jk+github@digineo.de>
2021-04-10 13:44:44 +0200

    Write errors to stderr

--- cmd/youtubedr/main.go
+++ cmd/youtubedr/main.go
@@ -6,15 +6,12 @@ import (
 )
 
 func main() {
-	if err := rootCmd.Execute(); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
+	exitOnError(rootCmd.Execute())
 }
 
 func exitOnError(err error) {
 	if err != nil {
-		fmt.Println(err)
+		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 }
