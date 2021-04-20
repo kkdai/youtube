e108b31a62acc9e2e60243e4b1932a5273b6620e
Minh-Quan TRAN <account@itscaro.me>
2021-04-20 12:16:57 +0200

    format for list cmd

--- cmd/youtubedr/list.go
+++ cmd/youtubedr/list.go
@@ -1,27 +1,39 @@
 package main
 
 import (
+	"encoding/json"
+	"encoding/xml"
 	"fmt"
+	"io"
 	"os"
 
 	"github.com/olekukonko/tablewriter"
 	"github.com/spf13/cobra"
 )
 
+type PlaylistInfo struct {
+	Title  string
+	Author string
+	Videos []VideoInfo
+}
+
+type playlistOutputWriter func(PlaylistInfo, io.Writer) error
+
 var (
-
-	// listCmd represents the list command
-	listCmd = &cobra.Command{
-		Use:   "list",
-		Short: "Print metadata of the desired playlist",
-		Args:  cobra.ExactArgs(1),
-		Run: func(cmd *cobra.Command, args []string) {
-			playlist, err := getDownloader().GetPlaylist(args[0])
-			exitOnError(err)
-
-			fmt.Println("Title:      ", playlist.Title)
-			fmt.Println("Author:     ", playlist.Author)
-			fmt.Println("# Videos:   ", len(playlist.Videos))
+	playlistOutputFunc    playlistOutputWriter
+	playlistOutputWriters = map[string]playlistOutputWriter{
+		"json": func(info PlaylistInfo, w io.Writer) error {
+			encoder := json.NewEncoder(w)
+			encoder.SetIndent("", "  ")
+			return encoder.Encode(info)
+		},
+		"xml": func(info PlaylistInfo, w io.Writer) error {
+			return xml.NewEncoder(w).Encode(info)
+		},
+		"plain": func(info PlaylistInfo, w io.Writer) error {
+			fmt.Println("Title:      ", info.Title)
+			fmt.Println("Author:     ", info.Author)
+			fmt.Println("# Videos:   ", len(info.Videos))
 
 			fmt.Println()
 
@@ -29,8 +41,7 @@ var (
 			table.SetAutoWrapText(false)
 			table.SetHeader([]string{"ID", "Author", "Title", "Duration"})
 
-			for _, vid := range playlist.Videos {
-
+			for _, vid := range info.Videos {
 				table.Append([]string{
 					vid.ID,
 					vid.Author,
@@ -39,11 +50,43 @@ var (
 				})
 			}
 			table.Render()
+			return nil
+		},
+	}
+	// listCmd represents the list command
+	listCmd = &cobra.Command{
+		Use:   "list",
+		Short: "Print metadata of the desired playlist",
+		Args:  cobra.ExactArgs(1),
+		PreRunE: func(cmd *cobra.Command, args []string) error {
+			playlistOutputFunc = playlistOutputWriters[outputFormat]
+			if playlistOutputFunc == nil {
+				return fmt.Errorf("output format %s is not valid", outputFormat)
+			}
+			return nil
+		},
+		Run: func(cmd *cobra.Command, args []string) {
+			playlist, err := getDownloader().GetPlaylist(args[0])
+			exitOnError(err)
+
+			playlistInfo := PlaylistInfo{
+				Title:  playlist.Title,
+				Author: playlist.Author,
+			}
+			for _, v := range playlist.Videos {
+				playlistInfo.Videos = append(playlistInfo.Videos, VideoInfo{
+					ID:       v.ID,
+					Title:    v.Title,
+					Author:   v.Author,
+					Duration: v.Duration.String(),
+				})
+			}
+			exitOnError(playlistOutputFunc(playlistInfo, os.Stdout))
 		},
 	}
 )
 
 func init() {
-	infoCmd.Flags()
 	rootCmd.AddCommand(listCmd)
+	addFormatFlag(listCmd.Flags())
 }
