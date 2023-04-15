fbf9cf639087a72d73725f629b70025ab1ee37d4
Julian Kornberger <jk+github@digineo.de>
2023-04-15 06:42:50 +0200

    Create folder for artifacts

--- artifacts.go
+++ artifacts.go
@@ -10,8 +10,15 @@ import (
 var artifactsFolder = os.Getenv("ARTIFACTS")
 
 func writeArtifact(name string, content []byte) {
+	// Ensure folder exists
+	err := os.MkdirAll(artifactsFolder, os.ModePerm)
+	if err != nil {
+		log.Printf("unable to create artifacts folder %s: %s", artifactsFolder, err)
+		return
+	}
+
 	path := filepath.Join(artifactsFolder, name)
-	err := os.WriteFile(path, content, 0600)
+	err = os.WriteFile(path, content, 0600)
 	if err != nil {
 		log.Printf("unable to write artifact %s: %v", path, err)
 	} else {
